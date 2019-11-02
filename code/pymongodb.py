5d2f69ebe1b0875a595dddb9dd23c13158f45097
Bhavani Jaladanki <bhavani@lawn-128-61-13-22.lawn.gatech.edu>
2019-11-02 14:34:19 -0400

    db

---
+++ code/pymongodb.py
@@ -0,0 +1,17 @@
+import pymongo
+from pprint import pprint
+import json 
+
+client = pymongo.MongoClient("localhost", 27017)
+db = client['BDAProject']
+collection = db['YelpDatabase']
+
+with open('example_1.json') as f:
+	file_data = json.load(f)
+	print(file_data)
+collection.insert_one(file_data)
+cursor = db.collection.find({"attributes.RestaurantsTakeOut":"true"})
+for document in cursor:
+	pprint(document)
+
+client.close()
